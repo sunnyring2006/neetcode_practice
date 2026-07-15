110f015f548df3d4261b65ac411d104b6b4a8d22
Kimaya Patil <patilkimaya01@gmail.com>
2026-07-15 22:57:38 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> mpp;
+        for(auto it:strs){
+            string s = it;
+            sort(s.begin(), s.end());
+            mpp[s].push_back(it);
+        }
+        
+        vector<vector<string>> result;
+        for(auto it : mpp) {
+            result.push_back(it.second);
+        }
+        return result;
+    }
+};
