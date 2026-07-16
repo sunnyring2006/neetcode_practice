96488aa556235f6c652abbf8cac131bc8362bfc4
Kimaya Patil <patilkimaya01@gmail.com>
2026-07-16 19:12:31 +0530

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> ans;
+       
+
+        for(int i=0;i<n;i++){
+             int product=1;
+            for(int j=0;j<n;j++){
+                if(i != j){
+                    product=product * nums[j];
+                }
+            }
+            ans.push_back(product);
+        }
+        return ans;
+    }
+};
